e617a110fee4870d9a1a0cdd88f55e663a78a9a0
Stal <stalcha01@gmail.com>
2024-11-28 14:25:04 +0100

    Testons la connexion au site HAHA!

--- tests/AccueilTest.php
+++ tests/AccueilTest.php
@@ -7,18 +7,26 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AccueilTest extends WebTestCase
 {
-    public function testAccueilPage()
+    /*public function testAccueilPage()
     {
         $client = static::createClient();
         $client->request('GET', '/');
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
-    }
+    }*/
     public function testh1LoginPage(): void
     {
         $client = static::createClient();
-        $client->request('GET','/login');
-        $this->assertSelectorTextContains('h1','Veuillez vous connecter');
+        $crawler = $client->request('GET','/login');
+        $crawler = $client->submitForm('tests',[
+            'email' => 'robert@afpa.fr',
+            'password' => 'robert',
+        ]);
+        $crawler = $client->request('GET','/profil/');
+        
+        $this->assertResponseIsSuccessful();
 
+        $this->assertSelectorTextContains('h1', 'Vos informations');
+        
     }
 
    // public function test
